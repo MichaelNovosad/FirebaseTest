0a7f403a507b3dd5f445cbbe15259c503ea1ec5e
Michael Novosad <michael.novosad@outlook.com>
2021-09-21 17:09:09 +0300

    Initial Commit

---
+++ Shared/FirebaseTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseTestApp.swift
+//  Shared
+//
+//  Created by Michael Novosad on 21.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
